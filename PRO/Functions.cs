4c7a81cfa0a9650a6307e3cdbc13907366e6b6b7
hbsambo1231 <hsambo89410@uat.edu>
2025-09-20 23:56:51 -0700

    Add files via upload

---
+++ PRO/Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALCULATOR
+{
+    static internal class Functions
+    {
+        static double Add(double num1, double num2) 
+
+        {
+            return num1 + num2;
+        }
+        static double Substract(double num1, double num2)
+
+        {
+            return num1 - num2;
+        }
+        static double multiply(double num1, double num2)
+
+        {
+            return num1 * num2;
+        }
+        static double Devide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ PRO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CCALCULATOR_APP
+
+namespace CCALCULATOR_APP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(functions.Add(4, 10));
+            Console.WriteLine(functions.subtract10, 4));
+            Console.WriteLine(functions.multiply(4, 10));
+            Console.WriteLine(functions.divide(10, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
